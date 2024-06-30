ba7098f3b512a02de65329227097ba2dd24f8ecf
pnshimyumuremyi <2715323@collegelacite.ca>
2024-06-30 13:19:20 -0400

    Mise a jour de la page d'accueil
    Mise a jour de la page d'accueil

--- src/Pages/Accueil.jsx
+++ src/Pages/Accueil.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Accueil = () => {
   return (
-    <div>Accueil</div>
+    
+    <div className="accueil">
+      <div className="photo-container">
+        <img src="Pascal.png.jpg" alt="Pascal NSHIMYUMUREMYI" className="photo"/>
+      </div>
+      <div className="presentation">
+        <h1>Bienvenue sur mon portfolio</h1>
+        <p>Je suis <strong>Pascal NSHIMYUMUREMYI</strong>, diplômé en programmation informatique avec une spécialisation en développement web et mobile. Passionné par les nouvelles technologies, j'ai acquis une solide expérience dans divers langages de programmation et frameworks.</p>
+        <p>Explorez mon portfolio pour découvrir mes projets et compétences, et n'hésitez pas à me contacter pour toute collaboration ou opportunité professionnelle.</p>
+      </div>
+    </div>
+    
+    
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './stylesheets/Footer.css'
 const Footer = () => {
   return (
-    <div className='Footer'>Footer</div>
+    <footer>
+    <p>© 2024 Pascal NSHIMYUMUREMYI Tous droits réservés.</p>
+    <a href="https://github.com/pnshimyumuremyi" target="_blank" rel="noopener noreferrer">GitHub</a>
+    <a href="https://www.linkedin.com/in/pascal-nshimyumuremyi-1839178b/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+  </footer>
+
   )
 }
 
